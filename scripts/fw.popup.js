2aa85ce2663132ac372106200b05c550a6bb5c5b
Niek Weevers <niekweevers@gmail.com>
2015-03-05 11:30:35 +0100

    Script structuur wijzigen

--- scripts/fw.popup.js
+++ scripts/fw.popup.js
@@ -1,9 +1,6 @@
 var FW = FW || {};
 
-/**
- * Popup regelen
- */
-(function() {
+FW.popup = ( function( window, undefined ) {
     var $popup_wrap = $('<div class="popup__overlay"></div>').addClass('visuallyhidden'),
         $popup_inner = $('<div class="popup__inner"></div>').appendTo( $popup_wrap ),
         $popup_top = $('<div class="popup__top"></div>').appendTo( $popup_inner ),
@@ -11,58 +8,66 @@ var FW = FW || {};
 
         $popup_title = $('<h5></h5>').appendTo( $popup_top );
 
-    $('<span class="popup__close">Close</span>').appendTo( $popup_top );
-
-    $popup_wrap.appendTo('body');
-
-    // Update popup content
-    FW.update_popup_content = function( htmlcontent, popuptitle ) {
-        if( !popuptitle ) popuptitle = '';
-
-        $popup_title.text(popuptitle);
-        $popup_content.html( htmlcontent );
-    };
-
-    // Open popup
-    FW.open_popup = function() {
-        $popup_wrap.removeClass('visuallyhidden');
-    };
-
-    // Close popup
-    FW.close_popup = function() {
+    function close() {
         $popup_wrap.addClass('visuallyhidden');
         $popup_content.empty();
+    }
+
+    function open() {
+        $popup_wrap.removeClass('visuallyhidden');
+    }
+
+    function updatePopupContent( htmlcontent, popuptitle ) {
+        if( !popuptitle ) popuptitle = '';
+
+        $popup_title.text( popuptitle );
+        $popup_content.html( htmlcontent );
+    }
+
+    function bindEvents() {
+        // Open popup on every popup link
+        $('body').on('click', '[data-popup]', function(e) {
+            var $this = $(this),
+                $elOnPage = $( $this.attr('href') );
+
+            // Als het om een element op de pagina gaat, bijv #element
+            if( $elOnPage.length > 0 ) {
+                updatePopupContent( $elOnPage.html(), $this.attr('title') );
+                open();
+            }
+            else {
+                // Url met ajax ophalen
+                $.ajax({
+                    url: $this.attr('href')
+                }).done(function( data ) {
+                    updatePopupContent( data, $this.attr('title') );
+                    open();
+                });
+            }
+
+            e.preventDefault();
+        });
+
+        // Met sluitknop popup sluiten
+        $popup_inner.on('click', '.popup__close', close);
+
+        // Met esape popup sluiten
+        $(document).keyup(function(e) {
+            // Escape
+            if( e.keyCode == 27 ) { close(); }
+        });
+    }
+
+    function init() {
+        $('<span class="popup__close">Close</span>').appendTo( $popup_top );
+        $popup_wrap.appendTo('body');
+
+        bindEvents();
+    }
+
+    return {
+        init: init
     };
+})( window );
 
-    // Open popup on every popup link
-    $('body').on('click', '[data-popup]', function(e) {
-        var $this = $(this),
-            $elOnPage = $( $this.attr('href') );
-
-        // Als het om een element op de pagina gaat, bijv #element
-        if( $elOnPage.length > 0 ) {
-            FW.update_popup_content( $elOnPage.html(), $this.attr('title') );
-            FW.open_popup();
-        }
-        else {
-            // Url met ajax ophalen
-            $.ajax({
-                url: $this.attr('href')
-            }).done(function( data ) {
-                FW.update_popup_content( data, $this.attr('title') );
-                FW.open_popup();
-            });
-        }
-
-        e.preventDefault();
-    });
-
-    // Met sluitknop popup sluiten
-    $popup_inner.on('click', '.popup__close', FW.close_popup);
-
-    // Met esape popup sluiten
-    $(document).keyup(function(e) {
-        // Escape
-        if( e.keyCode == 27 ) { FW.close_popup(); }
-    });
-})();
+FW.popup.init();
